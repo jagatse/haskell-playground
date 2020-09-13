c0e03dd9c5cae6a612c2e022c7d6d6cffec0cc32
Johan Agat <jagat@tibco.com>
2020-09-13 17:09:57 +0200

    Some comments and clarifications.

--- queens.hs
+++ queens.hs
@@ -10,15 +10,18 @@ showBoard cs = unlines $ map showRow cs
         size = length cs
         dots = repeat '-'
 
--- Determines if a Queen placed at row qr and column qc checks the square at r,c
-checks :: Int -> Int -> Int -> Int -> Bool
-checks qr qc r c = qc == c || (r-qr) == abs(c-qc)
 
 -- Computes all solutions to the n-queens problem. See https://en.wikipedia.org/wiki/Eight_queens_puzzle
 queens :: Int -> [[Int]]
 queens n = subQueens 1 (\r c -> False)
     where
+        -- The checked parameter is a function that given a row and col determines if position (row,col) is checked by a queen at a previous row. 
         subQueens qr checked
             | qr > n = [[]]
             | otherwise = [ qc : rest | qc <- filter (not . (checked qr)) [1..n], rest <- subQueens (qr + 1) $ both checked (checks qr qc)]
+        -- Combines two check functions.
         both ch1 ch2 r c = (ch1 r c) || (ch2 r c)
+        -- Determines if a Queen placed at row qr and column qc checks the square at r,c
+        -- Assumes that r > qr, which is the only way it is called.
+        checks :: Int -> Int -> Int -> Int -> Bool
+        checks qr qc r c = qc == c || (r-qr) == abs(c-qc)
